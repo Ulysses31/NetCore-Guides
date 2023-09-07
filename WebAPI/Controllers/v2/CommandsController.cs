ed134da96001d11bf1dc68abd82fbb6b5e1266ff
SLG\ciordanidis <ciordanidis@singularlogic.eu>
2023-09-07 17:03:41 +0300

    Add commands controller async

--- WebAPI/Controllers/v2/CommandsController.cs
+++ WebAPI/Controllers/v2/CommandsController.cs
@@ -6,13 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Models;
 
-namespace WebAPI.Controllers.v1
+namespace WebAPI.Controllers.v2
 {
   /// <summary>
-  /// CommandsController
+  /// CommandsController (async)
   /// </summary>
   [ApiController]
-  [ApiVersion("1.0")]
+  [ApiVersion("2.0")]
   [Route("api/v{version:apiVersion}/[controller]")]
   [Consumes("application/json", "application/xml")]
   [Produces("application/json", "application/xml")]
@@ -25,15 +25,14 @@ namespace WebAPI.Controllers.v1
     /// </summary>
     public CommandsController(CommandContext context) => this._context = context;
 
-
     /// <summary>
-    /// Get all commands
+    /// Get all commands async
     /// </summary>
-    /// <returns>IEnumerable of CommandDto</returns>
+    /// <returns>Task IEnumerable of CommandDto</returns>
     /// <remarks>
     /// Sample request:
     ///
-    ///	GET /api/v1/commands
+    ///	GET /api/v2/commands
     ///
     /// </remarks>
     /// <response code="200">Http OK</response>
@@ -43,25 +42,25 @@ namespace WebAPI.Controllers.v1
     /// <response code="406">Not Acceptable</response>
     /// <response code="500">Internal Server Error</response>
     [HttpGet]
-    [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(IEnumerable<CommandDto>), StatusCodes.Status200OK)]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(Task<ActionResult<IEnumerable<CommandDto>>>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public ActionResult<IEnumerable<CommandDto>> GetCommands()
+    public async Task<ActionResult<IEnumerable<CommandDto>>> GetCommands()
     {
-      return Ok(_context.CommandItems);
+      return await Task.FromResult(_context.CommandItems);
     }
 
     /// <summary>
-    /// Get command by id
+    /// Get command by id async
     /// </summary>
     /// <param name="id">int</param>
-    /// <returns>IEnumerable of CommandDto</returns>
     /// <remarks>
     /// Sample request:
     ///
-    ///	GET /api/v1/commands/{id}
+    ///	GET /api/v2/commands/{id}
     ///
     /// </remarks>
+    /// <returns>Task ActionResult CommandDto></returns>
     /// <response code="200">Http OK</response>
     /// <response code="204">No Content</response>
     /// <response code="400">Bad Request</response>
@@ -69,27 +68,27 @@ namespace WebAPI.Controllers.v1
     /// <response code="406">Not Acceptable</response>
     /// <response code="500">Internal Server Error</response>
     [HttpGet("{id}")]
-    [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(CommandDto), StatusCodes.Status200OK)]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(Task<ActionResult<CommandDto>>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public ActionResult<CommandDto> GetCommandById(int id)
+    public async Task<ActionResult<CommandDto>> GetCommandById(int id)
     {
-      var commandItem = _context.CommandItems.Find(id);
+      var commandItem = await _context.CommandItems.FindAsync(id);
 
       if (commandItem == null) return NotFound();
 
-      return Ok(commandItem);
+      return await Task.FromResult(commandItem);
     }
 
     /// <summary>
-    /// AddCommand
+    /// AddCommand async
     /// </summary>
     /// <param name="commandItem">CommandDto</param>
-    /// <returns>CommandDto</returns>
+    /// <returns>Task ActionResult  of CommandDto></returns>
     /// <remarks>
     /// Sample request:
     ///
-    ///	POST /api/v1/commands
+    ///	POST /api/v2/commands
     ///	{
     ///		"howTo": "string",
     ///		"platform": "string",
@@ -104,15 +103,15 @@ namespace WebAPI.Controllers.v1
     /// <response code="406">Not Acceptable</response>
     /// <response code="500">Internal Server Error</response>
     [HttpPost]
-    [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(CommandDto), StatusCodes.Status200OK)]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(Task<ActionResult<CommandDto>>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public ActionResult<CommandDto> AddCommand(CommandDto commandItem)
+    public async Task<ActionResult<CommandDto>> AddCommand(CommandDto commandItem)
     {
       if (commandItem == null) return BadRequest();
 
-      _context.CommandItems.Add(commandItem);
-      _context.SaveChanges();
+      await _context.CommandItems.AddAsync(commandItem);
+      await _context.SaveChangesAsync();
 
       return CreatedAtAction(
         "GetCommandById",
@@ -122,7 +121,7 @@ namespace WebAPI.Controllers.v1
     }
 
     /// <summary>
-    /// UpdateCommand
+    /// UpdateCommand async
     /// </summary>
     /// <param name="id">int</param>
     /// <param name="commandItem">CommandDto</param>
@@ -130,7 +129,7 @@ namespace WebAPI.Controllers.v1
     /// <remarks>
     /// Sample request:
     ///
-    ///	PUT /api/v1/commands/{id}
+    ///	PUT /api/v2/commands/{id}
     ///	{
     ///		"id": 0,
     ///		"howTo": "string",
@@ -146,29 +145,28 @@ namespace WebAPI.Controllers.v1
     /// <response code="406">Not Acceptable</response>
     /// <response code="500">Internal Server Error</response>
     [HttpPut("{id}")]
-    [MapToApiVersion("1.0")]
+    [MapToApiVersion("2.0")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public ActionResult<CommandDto> UpdateCommand(int id, CommandDto commandItem)
+    public async Task<ActionResult<CommandDto>> UpdateCommand(int id, CommandDto commandItem)
     {
       if ((commandItem == null) || (id != commandItem.Id)) return BadRequest();
 
       _context.Entry(commandItem).State = EntityState.Modified;
-      _context.SaveChanges();
+      await _context.SaveChangesAsync();
 
-      return NoContent();
+      return await Task.FromResult(NoContent());
     }
 
-
     /// <summary>
-    /// DeleteCommand
+    /// DeleteCommand async
     /// </summary>
     /// <param name="id">int</param>
-    /// <returns>CommandDto</returns>
+    /// <returns>Task ActionResult of CommandDto</returns>
     /// <remarks>
     /// Sample request:
     ///
-    ///	DELETE /api/v1/commands/{id}
+    ///	DELETE /api/v2/commands/{id}
     ///
     /// </remarks>
     /// <response code="200">Http OK</response>
@@ -178,19 +176,19 @@ namespace WebAPI.Controllers.v1
     /// <response code="406">Not Acceptable</response>
     /// <response code="500">Internal Server Error</response>
     [HttpDelete("{id}")]
-    [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(CommandDto), StatusCodes.Status200OK)]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(Task<ActionResult<CommandDto>>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public ActionResult<CommandDto> DeleteCommand(int id)
+    public async Task<ActionResult<CommandDto>> DeleteCommand(int id)
     {
-      var commandItem = _context.CommandItems.Find(id);
+      var commandItem = await _context.CommandItems.FindAsync(id);
 
       if (commandItem == null) return NotFound();
 
       _context.CommandItems.Remove(commandItem);
-      _context.SaveChanges();
+      await _context.SaveChangesAsync();
 
-      return Ok(commandItem);
+      return await Task.FromResult(commandItem);
     }
 
   }
